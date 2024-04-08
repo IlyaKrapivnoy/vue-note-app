f0fab74f499c9b3d14b98fad076c909f930d27b8
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2024-04-08 15:29:37 +0200

    add username to localStorage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,13 +4,28 @@
       <h1>My Notes</h1>
       <span class="top-section_counter">Total notes {{ notesCounter }}</span>
     </section>
+    <section class="greeting">
+      <h2 class="title">
+        Enter your username:
+        <input type="text" placeholder="Type here..." v-model="userName" />
+      </h2>
+    </section>
   </main>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { onMounted, ref, watch } from "vue";
 
 const notesCounter = ref(0);
+const userName = ref("");
+
+watch(userName, (newVal) => {
+  localStorage.setItem("userName", newVal);
+});
+
+onMounted(() => {
+  userName.value = localStorage.getItem("userName") || "";
+});
 </script>
 
 <style scoped lang="scss">
@@ -25,6 +40,19 @@ const notesCounter = ref(0);
 
   .top-section_counter {
     font-weight: 800;
+  }
+}
+
+.greeting {
+  input {
+    margin-left: 10px;
+    font-size: 16px;
+    font-weight: 700;
+    text-transform: uppercase;
+    border: none;
+    border-bottom: 1px solid #42b983;
+    padding-bottom: 5px;
+    outline: none;
   }
 }
 </style>
