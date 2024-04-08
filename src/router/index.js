5cbceed0fcda8e28bc6f57945e80824c0e56ca3a
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2024-04-08 19:43:40 +0200

    add future tasks

--- src/router/index.js
+++ src/router/index.js
@@ -8,9 +8,9 @@ const routes = [
     component: HomeView,
   },
   {
-    path: "/about",
-    name: "about",
-    component: () => import("../views/AboutView.vue"),
+    path: "/tasks",
+    name: "tasks",
+    component: () => import("../views/TasksView.vue"),
   },
 ];
 
--- src/views/TasksView.vue
+++ src/views/TasksView.vue
@@ -1,5 +1,18 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <h2>What to do next:</h2>
+    <ul>
+      <li v-for="(task, index) in tasks" :key="index">{{ task }}</li>
+    </ul>
   </div>
 </template>
+
+<script setup>
+const tasks = [
+  "Responsive: mobile + tablet",
+  "Separate component to smaller once",
+  "Use VueX and split it to modules",
+  "Structure styles (Storybook is a good idea)",
+  "And so on.",
+];
+</script>
