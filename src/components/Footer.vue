839678108fec8c196a6b6c0d4192ca5e69d05242
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2024-04-30 12:51:18 +0200

    add currentYear

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,19 @@
 <template>
-  <div>{{ footerText }}</div>
+  <div class="wrapper">{{ footerText }} / {{ currentYear }}</div>
 </template>
 
 <script setup>
+import { currentYear } from "@/helpers/functions";
+
 defineProps({
   footerText: String,
 });
 </script>
+
+<style lang="scss" scoped>
+.wrapper {
+  padding: 20px 0;
+  text-transform: uppercase;
+  font-size: 14px;
+}
+</style>
